f2d58d0099fd09bb7d0ee27ed658cae7a0c4e0ec
gurungd265 <gurungd265@gmail.com>
2025-08-22 15:16:56 +0900

    Update SecurityConfig.java

--- backend/src/main/java/com/example/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/example/backend/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://127.0.0.1:5174","https://calmarketon-n2fk857wb-gurungd265s-projects.vercel.app")); // React/Vite 개발 서버 주소
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://127.0.0.1:5174","https://calmarketon-evsgfvlkg-gurungd265s-projects.vercel.app")); // React/Vite 개발 서버 주소
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
